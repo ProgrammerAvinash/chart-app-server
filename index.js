96533f3cecdc4e05c9d52df238ca439b0252178b
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2024-04-24 13:22:34 +0530

    refractor : comments deleted

--- index.js
+++ index.js
@@ -13,18 +13,15 @@ app.get("/data", async (req, res) => {
       "https://docs.google.com/spreadsheets/d/1l7GstWHc69HPV0irSdvoMIyHgtufUPKsbtCiNw7IKR0/export?format=csv";
     const response = await axios.get(spreadsheetUrl);
 
-    // Split CSV data into rows
     const rows = response.data.split("\n");
 
-    // Extract headers from the first row
     const headers = rows[0].split(",");
 
-    // Extract data from subsequent rows and format as array of objects
     const formattedData = [];
     for (let i = 1; i < rows.length; i++) {
       const rowData = rows[i].split(",");
       const obj = {
-        Day: rowData[0].split("/").reverse().join("-"), // Format date as 'YYYY-MM-DD'
+        Day: rowData[0].split("/").reverse().join("-"),
         Age: rowData[1],
         Gender: rowData[2],
         features: {
@@ -33,13 +30,12 @@ app.get("/data", async (req, res) => {
           C: rowData[5],
           D: rowData[6],
           E: rowData[7],
-          F: rowData[8].trim(), // Remove any trailing whitespace
+          F: rowData[8].trim(),
         },
       };
       formattedData.push(obj);
     }
 
-    // Send the formatted data as JSON response
     res.json(formattedData);
   } catch (error) {
     console.error("Error fetching data:", error.message);
